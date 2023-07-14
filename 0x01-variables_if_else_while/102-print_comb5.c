7bcafc4e080990aeb87950d1079cbd1d17d6f4b3
root <ghazal.lsfr@google.com>
2023-07-14 14:32:13 -0700

    compb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,26 +8,29 @@
 
 int main(void)
 {
-    int i;
-    int j;
+        int i;
+        int j;
 
-    for (i = 0; i <= 99; i++)
+        for (i = 0; i <= 99; i++)
         {
-            for (j = i + 1; j <= 99; j++)
-            {
-                putchar((i / 10) + '0');
-                putchar((i % 10) + '0');
-                putchar(' ');
-                putchar((j / 10) + '0');
-                putchar((j % 10) + '0');
+		
+               for (j = i + 1; j <= 99; j++)
+               { 
 
-                if (i < 98 || j < 99)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
+               		 putchar((i / 10) + '0');
+               		 putchar((i % 10) + '0');
+             	         putchar(' ');
+                         putchar((j / 10) + '0');
+                         putchar((j % 10) + '0');
+
+                         if (i < 98 || j < 99)
+                         {
+                              putchar(',');
+                              putchar(' ');
+                         }
+               }
         }
+
             putchar('\n');
 
             return (0);
