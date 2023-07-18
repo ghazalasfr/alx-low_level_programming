3177ab51f67d8103ed09b29caa66e02f6c01fda8
root <ghazal.lsfr@google.com>
2023-07-18 10:54:24 -0700

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,11 @@
-#include <stdio.h>
+/**
+  * Description: These header files provide the functionalities needed
+  */
 
+
+int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
