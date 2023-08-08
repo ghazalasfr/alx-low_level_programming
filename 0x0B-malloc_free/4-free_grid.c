999f2d6cc4779c2ec00d87a07ac0b0480d46713b
root <ghazal.lsfr@google.com>
2023-08-08 07:28:05 -0700

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * free_grid - free grid
+  * @grid: grid
+  * @height: height
+  *
+  * Return: result
+  */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
+
