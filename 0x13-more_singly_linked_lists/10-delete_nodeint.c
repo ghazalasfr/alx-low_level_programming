8b12cc780c2d76f8f92b56a090302a2bdf030b9b
ghazalasfr <ghazal.lsfr@gmail.com>
2023-09-27 15:44:26 -0700

    fixed

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,26 +5,46 @@
 /**
   * delete_nodeint_at_index - to delete node
   * @head: the head
-  * @idx: index
+  * @index: index
   *
-  * Return: listint_t
+  * Return: -1 failed 1 succeed
   */
-int delete_nodeint_at_index(listint_t **head, unsigned int idx)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-        listint_t *temp=head;
-        int i;
-        if (idx == 0) {
-                head = head -> next;
-                temp ->next = NULL;
-                free(temp);
-        }else {
-        for (i = 0; i < idx - 1; i++) {
-            temp = temp->next;
-        }
-         struct listint_t * del = temp->next;
-        temp->next = temp->next->next;
-        del->next = NULL;
-        free(del);
-        }
-        return (NULL);
+	listint_t *liste;
+	listint_t *tmp;
+
+	unsigned int i = 0;
+
+	list = *head;
+
+	if (*head == NULL || head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(liste);
+		return (1);
+	}
+
+	while (liste)
+	{
+		if (i == index - 1)
+		{
+			tmp = liste->next;
+
+			if (tmp == NULL)
+				return (-1);
+
+			liste->next = tmp->next;
+			free(tmp);
+			return (1);
+		}
+
+		i++;
+		liste = liste->next;
+	}
+
+	return (-1);
 }
