a4ee955c13c7dcf01302e76c389b07bd81dc33f3
ghazalasfr <ghazal.lsfr@gmail.com>
2023-09-28 09:28:41 -0700

    betty style fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,27 +3,30 @@
 #include <string.h>
 
 /**
-  * binary_to_uint - Converts a binary number to an unsigned int
-  * @b: The binary string to converts
-  *
-  * Return: The positive number converted from a binary
-  */
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: The binary string to convert.
+ *
+ * Return: The positive number converted from a binary.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int dec = 0, temp = 0, nb = 0;
 
-unsigned int binary_to_uint(const char *b){
-    unsigned int dec =0, temp=0,nb=0;
-    if(b==NULL) 
-    return (0);
-    nb=strlen(b);
-   while (nb--)
-	{
-		if (b[nb] != 48 && b[nb] != 49)
-			return (0);
+    if (b == NULL)
+        return (0);
 
-		if (b[nb] == 49)
-			dec += 1 << temp;
+    nb = strlen(b);
+    while (nb--)
+    {
+        if (b[nb] != '0' && b[nb] != '1')
+            return (0);
 
-		temp++;
-	}
+        if (b[nb] == '1')
+            dec += 1 << temp;
 
-	return (dec);
+        temp++;
+    }
+
+    return (dec);
 }
+
