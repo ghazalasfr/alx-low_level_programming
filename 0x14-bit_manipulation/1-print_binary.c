181603906c1935a22fce735d23463a445926e7bf
ghazalasfr <ghazal.lsfr@gmail.com>
2023-09-28 09:32:27 -0700

    betty style

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,22 +3,24 @@
 #include "main.h"
 
 /**
-  * print_binary - Prints the binary representation of a number
-  * @n: The number to representing in binary
-  *
-  * Return: Nothing
-  */
-
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to represent in binary.
+ *
+ * Return: Nothing.
+ */
 void print_binary(unsigned long int n)
 {
-    if (n==0 ) _putchar('0');
-    while (n) {
-    if (n & 1)
-        _putchar('1');
-    else
+    if (n == 0)
         _putchar('0');
+    
+    while (n)
+    {
+        if (n & 1)
+            _putchar('1');
+        else
+            _putchar('0');
 
-    n >>= 1;
-   }
-
+        n >>= 1;
+    }
 }
+
