4b27b12d9fc90874e0e621002fd1727376ce7bb8
ghazalasfr <ghazal.lsfr@gmail.com>
2023-09-29 11:54:10 +0100

    tets

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,10 @@
   * Return: 1 if it worked, or -1 if an error occurred
   */
 
-int clear_bit(unsigned long int *n, unsigned int index){
-    if (index < sizeof(unsigned long int) * 8)
-    {
+int clear_bit(unsigned long int *n, unsigned int index) 
+{
+	if (index < sizeof(unsigned long int) * 8) 
+	{
 		*n &= (~(1 << index));
 		return (1);
 	}
