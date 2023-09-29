829b0b8ec7c5ee82702daae14f1191983a143f82
ghazalasfr <ghazal.lsfr@gmail.com>
2023-09-29 12:44:55 +0100

    test

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,17 +6,16 @@
  *
  * Return: 1 for little endian, 0 for big endian.
  */
+
 int get_endianness(void)
 {
     unsigned int num = 1;
+
     char *byte_ptr = (char *)&num;
 
-    if (*byte_ptr == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
+    if (*byte_ptr == 1) {
+	    return 1;
+    } else {
+	    return 0;
     }
 }
