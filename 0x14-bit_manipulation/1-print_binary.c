7c8bebfdbbb4e72f9f4f459458995c21b6ee52f4
ghazalasfr <ghazal.lsfr@gmail.com>
2023-09-29 11:14:49 +0100

    test

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,14 +11,13 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+	if (n < 1) return;
+
+	print_binary( n >> 1);
+
+	if (n & 1)
+		_putchar('1');
+	else
 		_putchar('0');
-	while (n)
-	{
-		if (n & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-	n >>= 1;
-	}
+	
 }
