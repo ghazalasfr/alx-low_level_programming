e0f87840df0d733e75a3cf749b2d8510ebe08c91
ghazalasfr <ghazal.lsfr@gmail.com>
2023-09-29 10:23:03 +0100

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,18 +10,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec = 0, temp = 0, nb = 0;
+	unsigned int dec = 0,temp = 0,nb = 0;
 	if (b == NULL)
-        return (0);
+		return (0);
 	nb = strlen(b);
 	while (nb--)
 	{
-        if (b[nb] != '0' && b[nb] != '1')
-            return (0);
-
-        if (b[nb] == '1')
-            dec += 1 << temp;
-
+		if (b[nb] != '0' && b[nb] != '1')
+			return (0);
+		if (b[nb] == '1')
+			dec += 1 << temp;
 	temp++;
 
 	}
