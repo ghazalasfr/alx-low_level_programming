c06e963b82bec8660eb06fdc3bdd8155684a0468
ghazalasfr <ghazal.lsfr@gmail.com>
2023-10-02 18:09:13 +0100

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,23 +15,21 @@
   */
 int create_file(const char *filename, char *text_content) 
 {
-	 if (filename == NULL)
-		 return (-1);
-	 int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	 int fd, c= 0;
 
-	 if (fd == -1)i
-		 return (-1);
+	if (!filename)
+		return (-1);
 
-	 if (text_content != NULL) {
-		 size_t len = strlen(text_content);
-		 ssize_t bytes_written = write(fd, text_content, len);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 
-		 if (bytes_written == -1) {
-			 close(fd);
-			 return (-1);
-		 }
-	 }
+	while (text_content[c])
+		c++;
+               
+	if (text_content)
+		write(fd, text_content, C);
 
-	 close(fd);
-	 return (1);
+	close(fd);
+	return (1);
 }
