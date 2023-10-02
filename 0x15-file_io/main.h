500ff113b4dfcc36eeae418bd933d2fd3ffd7282
ghazalasfr <ghazal.lsfr@gmail.com>
2023-10-02 16:54:55 +0100

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _FILE_IO
+#define _FILE_IO
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
