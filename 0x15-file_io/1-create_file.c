be3102f8f96339bc0efc81a811dd5ba7f147fbab
ghazalasfr <ghazal.lsfr@gmail.com>
2023-10-02 23:41:43 +0100

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,9 @@
   *
   * Return: ...
   */
-int create_file(const char *filename, char *text_content) 
+int create_file(const char *filename, char *text_content)
 {
-	 int file;
-	 int len = 0;
+	 int file, len = 0;
 
 	if (filename == NULL)
 		return (-1);
