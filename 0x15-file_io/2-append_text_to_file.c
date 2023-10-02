8c3f424a2696f3989e57bd6bcec4d158129e6b93
ghazalasfr <ghazal.lsfr@gmail.com>
2023-10-03 00:00:39 +0100

    done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,25 +16,24 @@
   */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, c = 0;
+	int fille, c = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	fille = open(filename, O_WRONLY | O_APPEND);
+	if (fille == -1)
 		return (-1);
 
 	
 	while (text_content[c])
 		c++;
 
-	if (text_content)
-	{
-		if (write(fd, text_content, c) == -1)
+	if (text_content) {
+		if (write(fille, text_content, c) == -1)
 			return (-1);
 	}
 
-	close(fd);
+	close(fille);
 	return (1);
 }
