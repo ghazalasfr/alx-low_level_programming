e79075e650f3d1d5ed484d5ae0f048ff1920383a
ghazalasfr <ghazal.lsfr@gmail.com>
2023-10-02 18:10:22 +0100

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		c++;
                
 	if (text_content)
-		write(fd, text_content, C);
+		write(fd, text_content, c);
 
 	close(fd);
 	return (1);
