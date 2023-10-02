4c193bc1e426d944a954d6952eb1fba36711dcdd
ghazalasfr <ghazal.lsfr@gmail.com>
2023-10-02 17:41:42 +0100

    doen

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,13 +15,13 @@
   */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, readedlettres;
+	int file, readedletters;
 	char *buffer = malloc(sizeof(char *) * letters);
 
-	if (!buffer)
+	if (buffer ==NULL)
 		return (0);
 
-	if (!filename)
+	if (filename ==NULL)
 		return (0);
 
 	file = open(filename, O_RDONLY, 0600);
