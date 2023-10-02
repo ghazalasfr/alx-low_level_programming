4c5a6472b5e1ccf1ae51418f5ca67a1aa6c94b17
ghazalasfr <ghazal.lsfr@gmail.com>
2023-10-03 00:10:19 +0100

    done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,7 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		c++;
 
 	if (text_content) {
-		if (write(fille, text_content, c) == NULL)
+		if (write(fille, text_content, c) == 1)
 			return (-1);
 	}
 
