7dad3863831fa257554a624161f49b4417e12c6f
ghazalasfr <ghazal.lsfr@gmail.com>
2023-10-03 10:20:18 +0100

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -63,14 +63,14 @@ void copy_file(const char *src, const char *dest)
   * Return: ...
   */
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
+	
 	copy_file(argv[1], argv[2]);
 	exit(0);
 }
